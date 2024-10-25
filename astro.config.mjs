be5cb7e6dde905a847779c6006800a883abe1daa
bflorestal <yayou7710@hotmail.fr>
2024-10-25 19:23:32 +0200

    chore: updating dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
@@ -7,9 +7,6 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), image()]
+  integrations: [mdx(), tailwind()],
 });
