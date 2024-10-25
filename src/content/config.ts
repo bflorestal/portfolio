05b7b4a38e6fa6208adb7556f0020257c866a211
bflorestal <yayou7710@hotmail.fr>
2024-10-25 20:33:02 +0200

    chore: moved project images to `src/content/projects`

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,9 +12,11 @@ export const collections = {
     }),
   }),
   projects: defineCollection({
-    schema: z.object({
+    schema: ({ image }) => z.object({
       name: z.string(),
-      image: z.string(),
+      image: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
       technos: z.array(z.string()),
       repo: z.string().url().optional(),
       link: z.string().url().optional(),
