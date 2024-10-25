4b6a8c9efb2103d0860ef5598cf3f43074fe036b
bflorestal <yayou7710@hotmail.fr>
2024-10-25 20:31:54 +0200

    chore: updated astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import mdx from "@astrojs/mdx";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), icon()],
 });
