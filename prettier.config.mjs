fdefe1796206cf78f6c1e774c24dbee33d2c58d1
bflorestal <yayou7710@hotmail.fr>
2024-10-25 19:32:40 +0200

    chore: added prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,33 @@
+/**
+ * @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions &
+ *       import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
+ */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  astroAllowShorthand: false,
+  tailwindConfig: "./tailwind.config.cjs",
+  importOrder: ["<THIRD_PARTY_MODULES>", "", "^$lib/", "^[.][.]/", "^[.]/"],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
+};
+
+export default config;
