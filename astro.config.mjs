8ee3559df37f1610ea1d9cf8a766338979f1ff2c
bflorestal <yayou7710@hotmail.fr>
2024-10-25 19:34:54 +0200

    chore: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from "astro/config";
-
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
